a6900c9f62b42994a3e0baaaaae227e80662dd35
Hassan Rahman <engr.hassanrahman@gmail.com>
2022-11-17 18:00:05 +0500

    AppCenter.WPF added

---
+++ AppCenter.WPF/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace AppCenter.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            Microsoft.AppCenter.AppCenter.Start(
+                appSecret: "f85990b6-6d4d-4789-b8b1-1aa62e98d8a0",
+                typeof(Analytics),
+                typeof(Crashes));
+        }
+    }
+}
---
+++ AppCenter.WPF/MainWindow.xaml.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AppCenter.WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            label.Content = "Button Clicked";
+        }
+
+        private void BtnLogEvent_Click(object sender, RoutedEventArgs e)
+        {
+            Analytics.TrackEvent("Button clicked");
+            label.Content = "Event Tracked";
+        }
+
+        private void BtnLogException_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                throw new Exception();
+            }
+            catch (Exception exception)
+            {
+                Crashes.TrackError(exception);
+                label.Content = "Exception Logged";
+            }
+        }
+
+        private void BtnLogExceptionWithInfo_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                throw new Exception();
+            }
+            catch (Exception exception)
+            {
+                var properties = new Dictionary<string, string>
+                                    {
+                                        { "Category", "Music" },
+                                        { "Wifi", "On"}
+                                    };
+                Crashes.TrackError(exception, properties);
+                label.Content = "Exception Logged with information";
+            }
+        }
+
+        private void BtnGenerateCrash_Click(object sender, RoutedEventArgs e)
+        {
+            Crashes.GenerateTestCrash();
+        }
+
+        private async void BtnGetCrashAnalysis_Click(object sender, RoutedEventArgs e)
+        {
+            bool didAppCrash = await Crashes.HasCrashedInLastSessionAsync();
+            if (didAppCrash)
+            {
+                ErrorReport crashReport = await Crashes.GetLastSessionCrashReportAsync();
+                label.Content = $"didAppCrash:{didAppCrash}. crashReport:{crashReport.Device.OemName} | {crashReport.StackTrace}"; 
+            }
+            else
+            {
+                label.Content = $"App didn't crashed";
+            }
+        }
+    }
+}
